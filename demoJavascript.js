3f20e0f9fe913cb1574edd1b05cb08a6748e09e6
Cameron Carlyon <cameroncarlyon@gmail.com>
2023-09-10 17:23:20 +0100

    Update demoJavascript.js
    Further JS revisions.

--- demoJavascript.js
+++ demoJavascript.js
@@ -1,5 +1,6 @@
-console.log("Our opening times are between 9AM and 5PM");
-//checking if current time is between 9AM and 5PM
-var currentTime = 954;
+console.log("Our opening times are between 9 AM and 5 PM");
+//checking if current time is between 9 AM and 5 PM
+var currentTime = 800;
 if (currentTime > 899 && currentTime < 1700)
   console.log("We are currently open!");
+else console.log("We are currently closed.");
