0286e3cf2a3ee400a059a0df9b57c01560b218b2
Cameron Carlyon <cameroncarlyon@gmail.com>
2023-09-11 20:50:41 +0100

    Update demoJavascript.js
    Additional JS

--- demoJavascript.js
+++ demoJavascript.js
@@ -23,3 +23,52 @@ var result1 = test1 == 0;
 var result2 = test2 == 0;
 console.log("Is", num1, "an even number?", result1);
 console.log("Is", num2, "an even number?", result2);
+
+// {let example = "sample text"}
+// console.log(example)
+
+var example = "sample text 1";
+{
+  let example = "sample text 2";
+  console.log(example);
+}
+console.log(example);
+
+// Where do you get your money from
+var age = 10;
+if (age >= 65) {
+  console.log("You get your income from your pension");
+} else if ((age < 65, age > 18)) {
+  console.log("Each month you get a salary");
+} else if (age < 18) {
+  console.log("You get an allowance");
+} else {
+  console.log("The value of the age variable is not numerical");
+}
+
+var day = "Sunday";
+switch (day) {
+  case "Monday":
+    console.log("Do something");
+    break;
+  case "Tuesday":
+    console.log("Do something");
+    break;
+  case "Wednesday":
+    console.log("Do something");
+    break;
+  case "Thursday":
+    console.log("Do something");
+    break;
+  case "Friday":
+    console.log("Do something");
+    break;
+  case "Saturday":
+    console.log("Do something");
+    break;
+  case "Sunday":
+    console.log("Rest day");
+    break;
+  default:
+    console.log("There is no such day");
+}
