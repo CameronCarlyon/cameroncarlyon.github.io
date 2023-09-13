d5e5e7df653684e68f85032378cce1026e50405a
Cameron Carlyon <cameroncarlyon@gmail.com>
2023-09-13 22:44:13 +0100

    Update demoJavascript.js

--- demoJavascript.js
+++ demoJavascript.js
@@ -103,3 +103,29 @@ while (year <= 2022) {
   console.log(year);
   year++;
 }
+
+for (var i = 1; i <= 10; i++) {
+  if (i == 1) {
+    console.log("Gold medal");
+  } else if (i == 2) {
+    console.log("Silver medal");
+  } else if (i == 3) {
+    console.log("Bronze medal");
+  } else console.log(i);
+}
+
+for (var i = 1; i <= 10; i++) {
+  switch (i) {
+    case 1:
+      console.log("Gold medal");
+      break;
+    case 2:
+      console.log("Silver medal");
+      break;
+    case 3:
+      console.log("Bronze medal");
+      break;
+    default:
+      console.log(i);
+  }
+}
