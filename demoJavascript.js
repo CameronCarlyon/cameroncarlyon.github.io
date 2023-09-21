04f95434f336c5d957e9de180399ad2171598ce4
Cameron Carlyon <cameroncarlyon@gmail.com>
2023-09-22 00:35:14 +0100

    Update demoJavascript.js

--- demoJavascript.js
+++ demoJavascript.js
@@ -156,6 +156,21 @@ function letterFinder(word, match) {
 }
 letterFinder("test", "t");
 
+// Exercise: Defensive programming
+function letterFinder(word, match) {
+  var condition1 = typeof word == "string" && word.length >= 2;
+  var condition2 = typeof match == "string" && match.length == 1;
+  if (condition1 && condition2 == true) {
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] == match) {
+        console.log("Found the", match, "at", i);
+      } else console.log("---No match found at", i);
+    }
+  } else console.log("Please pass correct arguments to the function.");
+}
+letterFinder(20, 10);
+letterFinder("cat", "c");
+
 // -- INCORRECT --
 // function letterFinder (word, match){
 //   for (var counter = 0; counter++; counter==word.length){
